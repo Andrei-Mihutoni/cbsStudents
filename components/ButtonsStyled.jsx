3127e8ee1e4a11480079e169883c424ae6f82274
Andrei Mihutoni <andrei.mihutoni@gmail.com>
2022-06-02 18:27:33 +0200

    styling discrover and screens

---
+++ components/ButtonsStyled.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from '@rneui/base';
+
+const RaisedButton = (props) => <Button raised {...props} />;
+
+export default RaisedButton;
--- screens/Screen1.tsx
+++ screens/Screen1.tsx
@@ -1,61 +1,102 @@
-import { useEffect, useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../App';
-import { add, addChatroom, deleteChatroom, fetchChatrooms, subtract, toggleHappy } from '../store/actions/ChatActions';
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../App";
+import { add, addChatroom, deleteChatroom, fetchChatrooms, subtract, toggleHappy } from "../store/actions/ChatActions";
+import RaisedButton from "../components/ButtonsStyled";
+import { Button } from "react-native-elements";
 
 const Screen1 = ({ navigation }: { navigation: any }) => {
-    const [text, onChangeText] = useState('');
+  const [text, onChangeText] = useState("");
 
-    const isHappy = useSelector((state: RootState) => state.chat.isHappy); // subscribing to the store's chat slice/part
-    const dispatch = useDispatch();
-    const numberOfIcecreams = useSelector((state: RootState) => state.chat.counter)
-    const chatrooms = useSelector((state: RootState) => state.chat.chatrooms);
+  const dispatch = useDispatch();
+  const chatrooms = useSelector((state: RootState) => state.chat.chatrooms);
+  // const isHappy = useSelector((state: RootState) => state.chat.isHappy); // subscribing to the store's chat slice/part
+  // const numberOfIcecreams = useSelector((state: RootState) => state.chat.counter);
 
-    useEffect(() => {
-        dispatch(fetchChatrooms())
-    }, []);
+  useEffect(() => {
+    dispatch(fetchChatrooms());
+  }, []);
 
-    console.log("chatrooms", chatrooms);
+  console.log("chatrooms", chatrooms);
 
-    const renderItem = ({ item }: { item: any }) => (
-        <TouchableOpacity>
-            <Text>{item.title}</Text>
-            <Button title="Delete this chatroom" onPress={() => dispatch(deleteChatroom(item.id))} />
-        </TouchableOpacity>
-    );
-
-
-    return (
-        <View>
-            <Text>I am screen 1</Text>
-            <Text>Is Christian happy? {isHappy.toString()}</Text>
-            <Text>How many icecreams should Christians children have {numberOfIcecreams}</Text>
-            <Button title="Go to screen 2" onPress={() => navigation.navigate('Screen2')} />
-            <Button title="Flip happy" onPress={() => dispatch(toggleHappy())} />
-
-            <Button title="Give Icecream" onPress={() => dispatch(add())} />
-            <Button title="Steal Icecream" onPress={() => dispatch(subtract())} />
-
-            <TextInput placeholder="Chatroom name"
-                style={styles.input}
-                onChangeText={onChangeText}
-                value={text} />
-
-            <Button title='Add chatroom' onPress={() => dispatch(addChatroom(text))} />
-
-            <FlatList data={chatrooms} renderItem={renderItem} />
+  const renderItem = ({ item }: { item: any }) => (
+    <TouchableOpacity style={styles.ActiveChats}>
+      <Text>{item.title}</Text>
+      <View style={styles.Container}>
+        <View style={styles.BtnContainer}>
+          <Button title="X" onPress={() => dispatch(deleteChatroom(item.id))} />
         </View>
-    );
-}
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View>
+      <RaisedButton title="Go to screen 1" onPress={() => navigation.navigate("Screen2")} />
+
+      <Text style={styles.AddChatsTitle}>Add a new chatrooms</Text>
+      <View style={styles.Container}>
+        <TextInput placeholder="Chatroom name" style={styles.input} onChangeText={onChangeText} value={text} />
+        <View style={styles.BtnContainer}>
+          <Button style={styles.btn} title="+" onPress={() => dispatch(addChatroom(text))} />
+        </View>
+      </View>
+
+      <View style={styles.ActiveChatsTitle}>
+        <Text>Active Chatrooms</Text>
+        <FlatList data={chatrooms} renderItem={renderItem} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
+  input: {
+    width: 300,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  BtnContainer: {
+    width: 80,
+  },
+
+  RedBtnContainer: {
+    width: 50,
+    backgroundColor: "#DDDDDD",
+  },
+  btn: {
+    width: 100,
+  },
+  Container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+
+  ActiveChats: {
+    margin: 12,
+    // marginRight: 50,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    backgroundColor: "#edebf7",
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    borderRadius: 5,
+    padding: 5,
+  },
+  ActiveChatsTitle: {
+    margin: 20,
+  },
+
+  AddChatsTitle: {
+    margin: 20,
+    marginBottom: -4,
+  },
 });
 
 export default Screen1;
